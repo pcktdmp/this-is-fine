321c94b7416904fa483c609cfc2f80618c738047
Serge van Namen <serge@se-cured.org>
2024-03-05 23:38:50 +0100

    Support more log levels

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Handy for mocking backends.
 
 ## Usage
 
-`docker run -p 8080:8080 -e THIS_IS_FINE_STDOUT_LOGGING=im-a-log-line ghcr.io/pcktdmp/this-is-fine:latest`
+`docker run -p 8080:8080 -e THIS_IS_FINE_STDOUT_LOGGING="INFO:info-line,ERROR:error-line,WARN:warning-line,DEBUG:debug-line ghcr.io/pcktdmp/this-is-fine:latest`
--- src/cmd/fine/fine.go
+++ src/cmd/fine/fine.go
@@ -9,19 +9,31 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// Check if the environment variable is not empty
 	// Read the environment variable
 	listStr := os.Getenv("THIS_IS_FINE_STDOUT_LOGGING")
+	// Check if the environment variable is not empty
 	if listStr != "" {
-		// Split the string based on the comma delimiter to get a slice of strings
-		list := strings.Split(listStr, ",")
-
-		// Iterate through the list and print each element
-		for _, item := range list {
-			log.Info(item)
+		logItems := strings.Split(listStr, ",")
+		for _, logItem := range logItems {
+			logMessages := strings.Split(logItem, ":")
+			if len(logMessages) != 2 {
+				log.Fatal("Please define log level via 'INFO:example-log-message'. Supported levels are: INFO, WARN, ERROR and DEBUG")
+			}
+			switch sev := logMessages[0]; sev {
+			case "INFO":
+				log.Info(logMessages[1])
+			case "WARN":
+				log.Warn(logMessages[1])
+			case "ERROR":
+				log.Error(logMessages[1])
+			case "DEBUG":
+				log.Debug(logMessages[1])
+			default:
+				log.Print(logMessages[1])
+			}
 		}
 	} else {
-		fmt.Println("Environment variable THIS_IS_FINE_STDOUT_LOGGING is not set or is empty.")
+		fmt.Println("Environment variable THIS_IS_FINE_STDOUT_LOGGING is not set or is empty. Allowed format: THIS_IS_FINE_STDOUT_LOGGING=<LEVEL>:<MESSAGE>,<LEVEL>:<MESSAGE>")
 	}
 
 }
