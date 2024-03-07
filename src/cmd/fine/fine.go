6074760c271185044cbf3733a404cda6d9109fe6
Serge van Namen <serge@se-cured.org>
2024-03-07 08:30:04 +0100

    If environment variable is not set, just log "I'm fine"

--- src/cmd/fine/fine.go
+++ src/cmd/fine/fine.go
@@ -11,31 +11,33 @@ var listStr = os.Getenv("THIS_IS_FINE_LOG_LINES")
 var logItems = strings.Split(listStr, ",")
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	for _, logItem := range logItems {
-		logMessages := strings.Split(logItem, ":")
-		if len(logMessages) != 2 {
-			log.Fatal("Please define log level via 'INFO:example-log-message'. Supported levels are: INFO, WARN, ERROR and DEBUG")
-		}
-		switch sev := logMessages[0]; sev {
-		case "INFO":
-			log.Info(logMessages[1])
-		case "WARN":
-			log.Warn(logMessages[1])
-		case "ERROR":
-			log.Error(logMessages[1])
-		case "DEBUG":
-			log.Debug(logMessages[1])
-		default:
-			log.Print(logMessages[1])
+	if listStr == "" {
+		log.Print("I'm fine!")
+	} else {
+		for _, logItem := range logItems {
+			logMessages := strings.Split(logItem, ":")
+			if len(logMessages) != 2 {
+				log.Fatal("Please define log level via 'INFO:example-log-message'. Supported levels are: INFO, WARN, ERROR and DEBUG")
+			}
+			switch sev := logMessages[0]; sev {
+			case "INFO":
+				log.Info(logMessages[1])
+			case "WARN":
+				log.Warn(logMessages[1])
+			case "ERROR":
+				log.Error(logMessages[1])
+			case "DEBUG":
+				log.Debug(logMessages[1])
+			default:
+				log.Print(logMessages[1])
+			}
 		}
 	}
 
 }
 
 func main() {
-	if listStr == "" {
-		log.Fatal("Environment variable THIS_IS_FINE_LOG_LINES is not set or is empty. Allowed format: THIS_IS_FINE_LOG_LINES=<LEVEL>:<MESSAGE>,<LEVEL>:<MESSAGE>")
-	}
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
